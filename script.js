8a21b1ca7c6902653ed7d1c2cd1ddedc15ea0716
brh4n <126130032+burhandjzr@users.noreply.github.com>
2026-03-19 22:07:03 +0700

    webv1

---
+++ script.js
@@ -0,0 +1,34 @@
+// COUNTER
+function animateCounter(id, target) {
+  let count = 0;
+  let speed = target / 100;
+
+  let interval = setInterval(() => {
+    count += speed;
+    if (count >= target) {
+      count = target;
+      clearInterval(interval);
+    }
+    document.getElementById(id).innerText = Math.floor(count);
+  }, 20);
+}
+
+// SCROLL ANIMATION
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+    if (top < window.innerHeight - 100) {
+      sec.style.opacity = 1;
+      sec.style.transform = "translateY(0)";
+    }
+  });
+});
+
+// LOAD
+window.onload = () => {
+  animateCounter("c1", 300);
+  animateCounter("c2", 236);
+  animateCounter("c3", 11952);
+};
